4132003bb41c81f04f9f8ce0a96bfdf7684db7f2
shubhampal12125-cmyk <shubhampal12125@gmail.com>
2026-04-16 22:05:39 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
-
+import 'package:flutter/material.dart';
+void main()=>runApp(const MyApp());
+class MyApp extends StatelessWidget{
+const MyApp({super.key});
+@override
+Widget build(BuildContext context){
+return MaterialApp(debugShowCheckedModeBanner:false,home:Scaffold(
+appBar:AppBar(title:const Text('KSss Health Care')),
+body:ListView(children:const[
+ListTile(title:Text('Membership')),
+ListTile(title:Text('Find Doctor')),
+ListTile(title:Text('Book Appointment')),
+])));
+}}
